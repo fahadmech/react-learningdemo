3693a0211ec1170ad407d0c4c58c79b4a241db93
fahadmech <fshaikh349@gmail.com>
2019-07-09 19:04:20 +0530

    styling active route

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import { Route, NavLink } from "react-router-dom";
+import NewPost from "./NewPost/NewPost";
+
+class Blog extends Component {
+  render() {
+
+    return (
+      <div>
+        <header className="Blog">
+          <nav>
+            <ul>
+              <li><NavLink 
+              to="/" 
+              exact 
+              activeClassName="my-active"
+              activeStyle={{
+                  color: "#fa923f",
+                  textDecoration: "underline"
+              }}>Home</NavLink></li>
+              <li><NavLink to={{
+                  pathname: "/new-post",
+                  hash: "#submit",
+                  search: "?quick-submit = true"
+              }}>New Post</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
